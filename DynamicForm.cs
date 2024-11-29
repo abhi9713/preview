375b6048fddb60053103d9ecb95c3979acbec03d
abhi9713 <114233568+abhi9713@users.noreply.github.com>
2024-11-29 22:55:56 +0530

    changes

---
+++ DynamicForm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Abhishek_Lodhi.Models
+{
+    public class DynamicForm
+    {
+        [Key]
+        public int Id { get; set; } // Form ID
+        public string Name { get; set; } // Form name
+        public List<DynamicFormField> Fields { get; set; } = new List<DynamicFormField>();
+        // public List<DynamicFormField> Fields { get; set; } = new(); // List of form fields
+    }
+}
---
+++ DynamicFormField.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Abhishek_Lodhi.Models
+{
+    public class DynamicFormField
+    {
+        [Key]
+        public int Id { get; set; } // Primary key
+        public string? Label { get; set; } // Field label
+        public string? Value { get; set; } // Field label
+        public string? FieldType { get; set; } // Type of the field (text, dropdown, etc.)
+
+        public List<string> Options { get; set; } = new List<string>(); // Options for dropdown, checkbox, or radio
+
+        //public List<string>? Options { get; set; } // Options for dropdown, checkbox, etc.
+        public string? SelectedOption { get; set; }
+    }
+}
